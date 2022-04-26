679b425257805f20c46a96baff9cda63f09f3745
PAUBRY <PAUBRY@P215976.reseau.cora>
2022-04-26 17:33:01 +0200

    Initialisation

---
+++ src/main/java/fr/pay/projet/service1/Service1Daemon.java
@@ -0,0 +1,13 @@
+package fr.pay.projet.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service1Daemon {
+
+	public static void main(String... args) {
+		SpringApplication.run(Service1Daemon.class, args);
+	}
+	
+}
